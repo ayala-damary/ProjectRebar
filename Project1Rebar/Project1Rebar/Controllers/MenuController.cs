9293e0a3e74b35ba10cf1a9985f71d5d73d5df40
ayaladamary <ayalad@darkeychana.com>
2023-11-01 16:57:28 +0200

    Update Cintrollers in this project

---
+++ Project1Rebar/Project1Rebar/Controllers/MenuController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project1Rebar.Models;
+using Project1Rebar.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Project1Rebar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuService;
+        public MenuController(IMenuService menuService)
+        {
+            this._menuService = menuService;
+        }
+        // GET: api/<MenuController>
+        [HttpGet]
+        public ActionResult<List<Shake>> Get()
+        {
+            return _menuService.GetShakes();
+        }
+
+        // GET api/<MenuController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Shake> Get(Guid id)
+        {
+            var shake = _menuService.GetShakeById(id);
+            if (shake == null) { NotFound($"Shake with Id={id} not found"); }
+            return shake;
+        }
+
+        // POST api/<MenuController>
+        [HttpPost]
+        public ActionResult<Shake> Post([FromBody] Shake shake)
+        {
+            Valid validation = new Valid();
+            if (!validation.shakeValid(shake))
+                return BadRequest(validation.shakeValid(shake));
+            _menuService.Create(shake);
+            return CreatedAtAction(nameof(Get), new { id = shake.Id }, shake);
+        }
+
+        // PUT api/<MenuController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, [FromBody] Shake shake)
+        {
+
+            var existingShake = _menuService.GetShakeById(id);
+            if (existingShake == null)
+            {
+                return NotFound($"Shake with Id={id} not found");
+            }
+            Valid validation = new Valid();
+            if (!validation.shakeValid(shake))
+                return BadRequest(validation.shakeValid(shake));
+            _menuService.UpdateShake(id, shake);
+            return NoContent();
+        }
+
+        // DELETE api/<MenuController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var shake = _menuService.GetShakeById(id);
+            if (shake == null) { NotFound($"Shake with Id:{id} not found"); }
+            _menuService.RemoveShake(shake.Id);
+            return Ok($"Shake with Id :{id} deleted");
+        }
+    }
+}
---
+++ Project1Rebar/Project1Rebar/Controllers/OrderController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project1Rebar.Models;
+using Project1Rebar.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Project1Rebar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            this._orderService = orderService;
+        }
+        // GET: api/<OrderController>
+        [HttpGet]
+        public ActionResult<List<Shake>> Get()
+        {
+            return _orderService.GetShakes();
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Shake> Get(Guid id)
+        {
+            var order = _orderService.GetShakeById(id);
+            if (order == null) { NotFound($"order with Id={id} not found"); }
+            return order;
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public ActionResult<Shake> Post([FromBody] Shake shake)
+        {
+            if (shake == null)
+            {
+                return NotFound($"Shake with Id={shake.Id} not found");
+            }
+            if (string.IsNullOrEmpty(shake.Name))
+            {
+                return BadRequest("Name must not be empty.");
+            }
+
+            _orderService.CreateShakeInOrder(shake);
+            return CreatedAtAction(nameof(Get), new { id = shake.Id }, shake);
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, [FromBody] Shake shake)
+        {
+            var existingShake = _orderService.GetShakeById(id);
+            if (existingShake == null)
+            {
+                return NotFound($"Shake with Id={id} not found");
+            }
+            if (shake == null)
+            {
+                return NotFound($"Shake with Id={shake.Id} not found");
+            }
+            if (string.IsNullOrEmpty(shake.Name))
+            {
+                return BadRequest("Name must not be empty.");
+            }
+            _orderService.UpdateOrderShake(id, shake);
+            return NoContent();
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var shake = _orderService.GetShakeById(id);
+            if (shake == null) { NotFound($"Shake with Id={id} not found"); }
+            _orderService.DeleteShake(shake.Id);
+            return Ok($"Shake with Id ={id} deleted");
+        }
+    }
+}
